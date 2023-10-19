3522e6865ac454026c16d356370e88b5268dc822
SmittawutPongsa <138877979+SmittawutPongsa@users.noreply.github.com>
2023-10-20 01:09:22 +0530

    "implement fix not finish" done enemy move and attack dont know how to pause to show enemy making decision

--- src/main/java/Fix/Main.java
+++ src/main/java/Fix/Main.java
@@ -2,6 +2,6 @@ package Fix;
 
 public class Main {
     public static void main(String[] args) {
-        MainWindow window = new MainWindow("C:\\Project HSRPG\\7-project\\", "MAP");
+        MainWindow window = new MainWindow("C:\\Project HSRPG\\7-project", "map1");
     }
 }
